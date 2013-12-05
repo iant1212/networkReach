ccb8b04e6071c574e5f3ce71ba40fc86d36755e3
Gregers Petersen <gregers@septima.dk>
2013-12-05 14:58:46 +0100

    Modified to take table, cost and geometry field as arguments

--- networkReach.sql
+++ networkReach.sql
@@ -1,86 +1,91 @@
-/*
---create the following TYPE in your database:
-CREATE TYPE networkReachType AS
+﻿/*
+--Returns the following :
+Table
 (
-  id integer,
-  the_geom geometry
+  id integer,			-- The edge's ID
+  geom geometry, 		-- The geometry (is partial if factor <> 1)
+  factor double precision 	-- How much of the original edge (based on length) did we take
 );
 */
 
 /*
 networkReach parameters:
-1: id of the starting point in the vertices_tmp table
-2: network reach distance (in units of the edge table cost column)
+1: The edge table
+2: Id of the starting point in the vertices_tmp table
+3: Network reach distance (in units of the edge table cost column)
 */
-
-CREATE OR REPLACE FUNCTION networkReach(integer, integer)
-RETURNS SETOF networkReachType AS
+CREATE OR REPLACE FUNCTION parkering_routing.networkReachPartial(edgeTable regclass, costfield character varying, geomfield character varying, startnode integer, networkReachDistance double precision)
+RETURNS TABLE(id integer, geom geometry, factor double precision) AS
 $BODY$
   DECLARE
-    startNode ALIAS FOR $1;
-    networkReachDistance ALIAS FOR $2;
-
-    networkReach networkReachType;
+	-- nothing to declare
   BEGIN
-  
+
+    -- Create a temporary table for the network whilst we process it
+    -- DROP on commit so we do not get problems reusing it again in this session
     CREATE TEMP TABLE tempNetwork
     (
       id integer NOT NULL,
-      the_geom geometry NOT NULL
-    );
-        
-    INSERT INTO tempNetwork(id, the_geom)
-    SELECT s.id edgeID, s.the_geom
+      geom geometry NOT NULL,
+      factor double precision NOT NULL
+    ) 
+    ON COMMIT DROP; 
+
+
+    EXECUTE '    
+    INSERT INTO tempNetwork(id, geom, factor)
+    SELECT et.id, et.' || format('%s',geomfield) || ' AS geom, 1 as factor
     FROM
         (SELECT id1,cost from pgr_drivingDistance(
-	  'SELECT id, source, target, cost FROM streets',
-	  startNode,networkReachDistance,false,false)
-	 ) firstPath
+          ''SELECT id, source, target, ' || quote_ident(costField) || ' AS cost FROM ' || format('%s',edgeTable) || ''',
+          ' || format('%s',startnode) || ',' || format('%s', networkReachDistance) || ',false,false)
+         ) firstPath
     CROSS JOIN 
         (SELECT id1,cost from pgr_drivingDistance(
-	  'SELECT id, source, target, cost FROM streets',
-	  startNode,networkReachDistance,false,false)
-	 ) secondPath
-    INNER JOIN streets s
-    ON firstPath.id1 = s.source
-    AND secondPath.id1 = s.target;
+          ''SELECT id, source, target, ' || quote_ident(costField) || ' AS cost FROM ' || format('%s',edgeTable) || ''',
+          ' || format('%s',startnode) || ',' || format('%s', networkReachDistance) || ',false,false)
+         ) secondPath
+    INNER JOIN ' || format('%s',edgeTable) || ' et 
+    ON firstPath.id1 = et.source
+    AND secondPath.id1 = et.target;';
 
-    INSERT INTO tempNetwork(id, the_geom)
-    SELECT allNodes.id, st_linesubstring(allNodes.the_geom, 0.0, (networkReachDistance-allNodes.distance)/allNodes.Cost) 
+    EXECUTE format('
+    INSERT INTO tempNetwork(id, geom, factor)
+    SELECT allNodes.id, st_line_substring(allNodes.geom, 0.0, (' || format('%s', networkReachDistance) || '-allNodes.distance)/allNodes.Cost), (' || format('%s', networkReachDistance) || '-allNodes.distance)/allNodes.Cost AS factor
     FROM
-      (SELECT reachNodes.id1, s1.id, s1.cost, reachNodes.cost distance, s1.the_geom
+      (SELECT reachNodes.id1, et.id, et.cost, reachNodes.cost distance, et.' || format('%s',geomfield) || '
        FROM
          (SELECT id1, cost FROM pgr_drivingDistance(
-	 'SELECT id, source, target, cost FROM streets',
-	 startNode,networkReachDistance,false,false)
-	 ) reachNodes
-      JOIN (SELECT id, target, source, cost, the_geom FROM streets) s1 ON reachNodes.id1 = s1.source
+         ''SELECT id, source, target, ' || quote_ident(costField) || ' AS cost FROM ' || format('%s',edgeTable) || ''',
+         ' || format('%s',startnode) || ',' || format('%s', networkReachDistance) || ',false,false)
+         ) reachNodes
+      JOIN (SELECT p.id, p.target, p.source, p.' || quote_ident(costField) || ' AS cost, p.geom FROM ' || format('%s',edgeTable) || ' p) et ON reachNodes.id1 = et.source
       ORDER BY reachNodes.id1
      ) allNodes
     FULL OUTER JOIN tempNetwork
     ON tempNetwork.id = allNodes.id
-    WHERE tempNetwork.id IS NULL;
+    WHERE tempNetwork.id IS NULL;', edgeTable);
 
-    INSERT INTO tempNetwork(id, the_geom)
-    SELECT allNodes.id, st_linesubstring(allNodes.the_geom,1-((networkReachDistance-allNodes.distance)/allNodes.Cost),1) 
+    EXECUTE format('
+    INSERT INTO tempNetwork(id, geom, factor)
+    SELECT allNodes.id, st_line_substring(allNodes.geom,1-((' || format('%s', networkReachDistance) || '-allNodes.distance)/allNodes.Cost),1), (' || format('%s', networkReachDistance) || '-allNodes.distance)/allNodes.Cost AS factor
     FROM
-      (SELECT reachNodes.id1, s1.id, s1.cost, reachNodes.cost distance, s1.the_geom
+      (SELECT reachNodes.id1, et.id, et.cost, reachNodes.cost distance, et.' || format('%s',geomfield) || '
        FROM
          (SELECT id1, cost FROM pgr_drivingDistance(
-	 'SELECT id, source, target, cost FROM streets',
-	 startNode,networkReachDistance,false,false)
-	 ) reachNodes
-       JOIN (SELECT id, target, source, cost, the_geom FROM streets) s1 ON reachNodes.id1 = s1.target 
+         ''SELECT id, source, target, ' || quote_ident(costField) || ' AS cost FROM ' || format('%s',edgeTable) || ''',
+         ' || format('%s',startnode) || ',' || format('%s', networkReachDistance) || ',false,false)
+         ) reachNodes
+       JOIN (SELECT p.id, p.target, p.source, p.' || quote_ident(costField) || ' AS cost, p.' || format('%s',geomfield) || ' FROM ' || format('%s',edgeTable) || ' p) et ON reachNodes.id1 = et.target
        ORDER BY reachNodes.id1
       ) allNodes
     FULL OUTER JOIN tempNetwork
     ON tempNetwork.id = allNodes.id
-    WHERE tempNetwork.id IS NULL;
+    WHERE tempNetwork.id IS NULL;', edgeTable);
 
-  FOR networkReach IN SELECT * FROM tempNetwork
-  LOOP
-    RETURN NEXT networkReach;
-  END LOOP;
+  
+    RETURN QUERY SELECT t.id, t.geom, t.factor FROM tempNetwork t;
+  
 END;
 $BODY$
   LANGUAGE plpgsql;
